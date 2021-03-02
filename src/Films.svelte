3e5f4e84a4fa29c199fd161968c75d3208227299
giomine <giomineo@gmail.com>
2021-03-02 13:05:10 +0000

    Improve fetch error message

--- src/Films.svelte
+++ src/Films.svelte
@@ -35,7 +35,7 @@ window.onload = () => {         // important! this defers the javascript so that
     app.appendChild(container)
 })
 .catch(error => {
-    console.log(error)
+    console.log('There has been a problem with your fetch operation: ', error)
 })
 }
 </script>
